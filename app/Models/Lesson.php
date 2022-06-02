d9d80ebfc50125b0b0b361ea2c3f169c282f0381
Mihailo Cvetkovic <info@mihailo.cz>
2022-06-02 10:23:25 +0200

    prev/next lesson

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -33,6 +33,11 @@ class Lesson extends Model
 //        'chapter'
     ];
 
+    protected $appends = [
+        'next_lesson',
+        'previous_lesson',
+    ];
+
     public function course()
     {
         return $this->belongsTo(Course::class);
@@ -61,5 +66,43 @@ class Lesson extends Model
     public function getIconAttribute()
     {
         return $this->type;
+    }
+
+    public function getPreviousLessonAttribute()
+    {
+        $prev = Lesson::where([
+            ['course_id', '=', $this->course_id],
+            ['id', '<', $this->id]
+        ])->orderBy('id', 'DESC');
+
+        if ($prev->exists()) {
+            $prev = $prev->first();
+            return [
+                'chapter_id' => $prev->chapter_id,
+                'slug' => $prev->slug,
+                'title' => $prev->title
+            ];
+        }
+
+        return null;
+    }
+
+    public function getNextLessonAttribute()
+    {
+        $next = Lesson::where([
+            ['course_id', '=', $this->course_id],
+            ['id', '>', $this->id]
+        ])->orderBy('id');
+
+        if ($next->exists()) {
+            $next = $next->first();
+            return [
+                'chapter_id' => $next->chapter_id,
+                'slug' => $next->slug,
+                'title' => $next->title
+            ];
+        }
+
+        return null;
     }
 }
